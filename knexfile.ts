df6c94657aff0727d4dcc0f06b03558efbfa6c87
Emmanuel Marcos de Oliveira <oliveirafullstack@gmail.com>
2026-08-01 00:02:04 -0300

    feat: add database and migration files for document management

--- knexfile.ts
+++ knexfile.ts
@@ -3,11 +3,11 @@ import type { Knex } from 'knex'
 const config: Knex.Config = {
   client: 'sqlite3',
   connection: {
-    filename: './src/database/app.db',
+    filename: './database/app.db',
   },
   useNullAsDefault: true,
   migrations: {
-    directory: './src/database/migrations',
+    directory: './database/migrations',
   },
 }
 export default config
