ce165504b9c498d2ae88ff1291be36ecf06d7500
Emmanuel Marcos de Oliveira <oliveirafullstack@gmail.com>
2026-08-13 23:34:10 -0300

    feat: add host configuration to server listener

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import {server} from './app.ts'
 import { env } from './env/index.ts'
 
-server.listen( {port: env.PORT} ).then(() => {
+server.listen( {port: env.PORT, host: '0.0.0.0',} ).then(() => {
     console.log(`🔥Server is test in http://localhost:${env.PORT}/api/health`)
 })
